1ca70a5f072a8c2520f6d03afd701ce23a5fef14
Navid Samanghoon <n.jsamanghoon@gmail.com>
2026-09-04 18:54:18 +0200

    Initial configuration

---
+++ model/config.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ModelConfig:
+        embedding_dim: int = 256
+        attention_dim: int = 64
+        max_sequence_length: int = 128
+        num_heads: int = 4
+        hidden_dim: int = 1024
+        num_layers: int = 4
